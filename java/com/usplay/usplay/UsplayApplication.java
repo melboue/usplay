cb913ca737f76dd0d45e9ad493a2402bcf5b8835
melboue <568908131@qq.com>
2019-04-23 10:13:20 +0800

    first

---
+++ java/com/usplay/usplay/UsplayApplication.java
@@ -0,0 +1,13 @@
+package com.usplay.usplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsplayApplication.class, args);
+	}
+
+}
